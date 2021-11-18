f036498cd7969fbc985e805771b2fe8607c65dc9
Dang Hau Nguyen <33993567+ngdanghau@users.noreply.github.com>
2021-11-18 18:11:19 +0700

    sửa sp

--- StoredProcedures/site_cntt_vt/sp_InDSLTC.sql
+++ StoredProcedures/site_cntt_vt/sp_InDSLTC.sql
@@ -1,20 +1,23 @@
 USE [QLDSV_TC]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_InDSLTC]    Script Date: 11/18/2021 6:09:47 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE OR ALTER PROCEDURE sp_InDSLopTinChi
-	-- khai bao cac bien tam 
+CREATE OR ALTER PROCEDURE [dbo].[sp_InDSLTC] 
+	-- Add the parameters for the stored procedure here
 	@NIENKHOA nchar(9), @HOCKY int
 AS
 BEGIN
 
-	SELECT mh.TENMH, ltc.NHOM, (gv.HO + ' ' + gv.TEN) as 'HO TEN', ltc.SOSVTOITHIEU, COUNT(dk.MASV) as soSVDK
+	SELECT  ROW_NUMBER() over(ORDER BY mh.TENMH, ltc.NHOM) STT, mh.TENMH, ltc.NHOM, (gv.HO + ' ' + gv.TEN) as 'HO TEN', ltc.SOSVTOITHIEU, COUNT(dk.MASV) as soSVDK
 	FROM  LOPTINCHI as ltc
 
 	LEFT JOIN GIANGVIEN as gv ON gv.MAGV = ltc.MAGV
@@ -26,4 +29,4 @@ BEGIN
 	ORDER BY mh.TENMH, ltc.NHOM
 	  
 END
-GO
+
