3af2b948c3fda0c0338d80192551a47ee569a0d0
Dang Hau Nguyen <33993567+ngdanghau@users.noreply.github.com>
2021-12-14 17:40:34 +0700

    sửa sp

--- StoredProcedures/site_cntt_vt/sp_InBangDiemTongKet.sql
+++ StoredProcedures/site_cntt_vt/sp_InBangDiemTongKet.sql
@@ -1,4 +1,6 @@
 USE [QLDSV_TC]
+GO
+/****** Object:  StoredProcedure [dbo].[sp_InBangDiemTongKet]    Script Date: 12/14/2021 5:38:17 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,18 +10,31 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE OR ALTER PROCEDURE sp_InBangDiemTongKet
+ALTER   PROCEDURE [dbo].[sp_InBangDiemTongKet]
 	-- khai bao cac bien tam 
 	@MALOP nchar(10)
 AS
 BEGIN
-	SELECT HOTEN = SV.MASV + ' - ' +SV.HO + ' ' + SV.TEN, mh.TENMH, MAX(dk.DIEM_CC *0.1 + dk.DIEM_CK * 0.6 + dk.DIEM_GK * 0.3) AS 'DIEM'
-	FROM SINHVIEN AS SV, DANGKY AS dk, MONHOC AS mh
-	WHERE 
-	SV.MALOP = @MALOP AND
-	SV.MASV = dk.MASV
-	GROUP BY sv.HO, sv.MASV, sv.TEN, mh.MAMH, mh.TENMH
-	ORDER BY sv.TEN, sv.HO ASC
+	SELECT HOTEN = SV.MASV + ' - ' +SV.HO + ' ' + SV.TEN, DIEM.TENMH, DIEM.DIEM
+FROM SINHVIEN AS SV
+INNER JOIN (SELECT MASV, DIEM = MAX(DIEM), TENMH
+			FROM 
+			(SELECT DK.MALTC, DK.MASV, DIEM = DK.DIEM_CC *0.1 + DK.DIEM_CK * 0.6 + DK.DIEM_GK * 0.3 
+			FROM DANGKY DK
+			GROUP BY DK.MALTC, DK.MASV, DK.DIEM_CC, DK.DIEM_CK, DK.DIEM_GK) AS DIEM
+
+			LEFT JOIN  (
+				SELECT LTC.MALTC, MH.TENMH
+				FROM MONHOC MH
+				INNER JOIN LOPTINCHI LTC
+				ON LTC.MAMH = MH.MAMH
+			) AS MH
+
+			ON DIEM.MALTC = MH.MALTC
+			GROUP BY MH.TENMH, DIEM.MASV) AS DIEM
+
+ON SV.MASV = DIEM.MASV  AND SV.MALOP = @MALOP
+GROUP BY sv.HO, sv.MASV, sv.TEN, DIEM.TENMH, DIEM.DIEM
+ORDER BY sv.TEN, sv.HO ASC
 
 END
-GO
--- StoredProcedures/site_cntt_vt/sp_InDSLTC.sql
+++ StoredProcedures/site_cntt_vt/sp_InDSLTC.sql
@@ -1,32 +1,23 @@
 USE [QLDSV_TC]
 GO
-/****** Object:  StoredProcedure [dbo].[sp_InDSLTC]    Script Date: 11/18/2021 6:09:47 PM ******/
+/****** Object:  StoredProcedure [dbo].[sp_InDSLTC]    Script Date: 14/12/2021 4:37:28 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
--- =============================================
--- Author:		<Author,,Name>
--- Create date: <Create Date,,>
--- Description:	<Description,,>
--- =============================================
-CREATE OR ALTER PROCEDURE [dbo].[sp_InDSLTC] 
-	-- Add the parameters for the stored procedure here
-	@NIENKHOA nchar(9), @HOCKY int
+ALTER PROCEDURE [dbo].[sp_InDSLTC] @NIENKHOA nchar(9), @HOCKY int
 AS
 BEGIN
 
 	SELECT  ROW_NUMBER() over(ORDER BY mh.TENMH, ltc.NHOM) STT, mh.TENMH, ltc.NHOM, (gv.HO + ' ' + gv.TEN) as 'HO TEN', ltc.SOSVTOITHIEU, COUNT(dk.MASV) as soSVDK
-	FROM  LOPTINCHI as ltc
+	
+	FROM  (SELECT ltc.MALTC, ltc.NIENKHOA, ltc.HOCKY, ltc.nhom, ltc.SOSVTOITHIEU , ltc.MAGV , ltc.MAMH , ltc.HUYLOP FROM LOPTINCHI as ltc WHERE ltc.NIENKHOA = @NIENKHOA AND ltc.HOCKY = @HOCKY AND ltc.HUYLOP = 0) as ltc
 
 	LEFT JOIN GIANGVIEN as gv ON gv.MAGV = ltc.MAGV
 	LEFT JOIN MONHOC as mh ON mh.MAMH = ltc.MAMH
 	LEFT JOIN DANGKY as dk ON dk.MALTC = ltc.MALTC
 
-	WHERE ltc.NIENKHOA = @NIENKHOA AND ltc.HOCKY = @HOCKY
 	GROUP BY ltc.NHOM, mh.TENMH, gv.HO, gv.TEN, ltc.SOSVTOITHIEU
 	ORDER BY mh.TENMH, ltc.NHOM
 	  
 END
-
