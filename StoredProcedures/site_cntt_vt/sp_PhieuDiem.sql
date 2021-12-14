50927afafe5dd45b453009eca3d9f3100d135942
Dang Hau Nguyen <33993567+ngdanghau@users.noreply.github.com>
2021-12-14 17:52:32 +0700

    sửa sp

--- StoredProcedures/site_cntt_vt/sp_PhieuDiem.sql
+++ StoredProcedures/site_cntt_vt/sp_PhieuDiem.sql
@@ -1,29 +1,28 @@
 USE [QLDSV_TC]
 GO
-/****** Object:  StoredProcedure [dbo].[sp_PhieuDiem]    Script Date: 11/18/2021 6:07:23 PM ******/
+/****** Object:  StoredProcedure [dbo].[sp_PhieuDiem]    Script Date: 14/12/2021 5:33:15 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 -- =============================================
 -- Author:		<Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE OR ALTER PROCEDURE [dbo].[sp_PhieuDiem]
+ALTER PROCEDURE [dbo].[sp_PhieuDiem]
 	-- khai bao cac bien 
 	@MASV char(12)
 AS
 BEGIN
    SELECT  ROW_NUMBER() over(ORDER BY mh.TENMH) STT, mh.TENMH , MAX(dk.DIEM_CC *0.1 + dk.DIEM_CK * 0.6 + dk.DIEM_GK * 0.3) AS 'DIEM'
-   FROM LOPTINCHI as ltc
-
+   
+   FROM (SELECT * from DANGKY as dk where dk.MASV = @MASV ) as dk
+	
+   INNER JOIN LOPTINCHI as ltc ON dk.MALTC= ltc.MALTC
    INNER JOIN MONHOC as mh ON mh.MAMH = ltc.MAMH
-   INNER JOIN DANGKY as dk ON dk.MALTC= ltc.MALTC
+   
 
-   WHERE dk.MASV = @MASV
    GROUP BY mh.TENMH
    ORDER BY mh.TENMH
 END
-
